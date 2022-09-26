f97dd60abf3462fe8611ccb6caa631a0a8f3a6f6
eExzoTiKe <podlozhnyy47work@gmail.com>
2022-09-26 16:14:53 +0300

    first commit

---
+++ MySite/news/views.py
@@ -0,0 +1,34 @@
+from urllib import request
+from webbrowser import get
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import News, Category
+from .forms import NewsForm
+
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, template_name='news/index.html', context = context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+
+    category = Category.objects.get(pk=category_id)
+    return render (request, 'news/index.html', {'news': news, 'category': category})
+
+
+def view_news(request, news_id):
+   # news_item = News.objects.get(pk=news_id)
+   news_item = get_object_or_404(News, pk=news_id)
+   return render(request, 'news/view_news.html', {"news_item": news_item})
+
+def add_news(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
